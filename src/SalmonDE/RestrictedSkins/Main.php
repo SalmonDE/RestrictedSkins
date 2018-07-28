9c997d09b27cdb68fbbe3e4a6dd09abb87583a50
SalmonDE <salmonde@salmonde.de>
2018-07-28 17:39:39 +0200

    Implement suggestion #2

--- src/SalmonDE/RestrictedSkins/Main.php
+++ src/SalmonDE/RestrictedSkins/Main.php
@@ -12,8 +12,21 @@ use pocketmine\utils\BinaryStream;
 
 class Main extends PluginBase implements Listener {
 
+    private $fallbackSkinData;
+
     public function onEnable(): void{
         $this->saveResource('config.yml');
+        $this->saveResource('fallback.png');
+
+        $fallbackSkin = new Skin('fallback', self::getSkinDataFromPNG($this->getDataFolder().'fallback.png'));
+
+        if(!$fallbackSkin->isValid()){
+            $this->getLogger()->error('Invalid skin supplied as fallback. Reverting to default one.');
+            $fallbackSkin = new Skin('fallback', self::getSkinDataFromPNG($this->getFile().'/resources/fallback.png'));
+        }
+
+        $this->fallbackSkinData = $fallbackSkin->getSkinData();
+
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
@@ -31,11 +44,15 @@ class Main extends PluginBase implements Listener {
         $event->setNewSkin($this->getStrippedSkin($event->getNewSkin()));
     }
 
+    public function getFallbackSkinData(): string{
+        return $this->fallbackSkinData;
+    }
+
     public function getStrippedSkin(Skin $skin): Skin{
-        $skinData = ($noCustomSkins = $this->getConfig()->get('disable-custom-skins') === \true) ? \str_repeat("\0xFF", 2048) : $skin->getSkinData();
+        $skinData = ($noCustomSkins = $this->getConfig()->get('disable-custom-skins') === \true) ? $this->fallbackSkinData : $skin->getSkinData();
 
         if(!$noCustomSkins && $this->getConfig()->get('disable-transparent-skins') === \true && $this->getSkinTransparencyPercentage($skinData) > $this->getConfig()->get('allowed-transparency')){
-            $skinData = \str_repeat("\0xFF", 2048);
+            $skinData = $this->fallbackSkinData;
         }
 
         $capeData = $this->getConfig()->get('disable-custom-capes') === \true ? '' : $skin->getCapeData();
@@ -64,7 +81,7 @@ class Main extends PluginBase implements Listener {
 
             default:
                 throw new InvalidArgumentException('Inappropriate skin data length: '.\strlen($skinData));
-    }
+        }
 
         $stream = new BinaryStream($skinData);
         $transparentPixels = 0;
@@ -83,5 +100,21 @@ class Main extends PluginBase implements Listener {
         }
 
         return (int) \round($transparentPixels * 100 / ($maxX * $maxY));
+    }
+
+    public static function getSkinDataFromPNG(string $path) : string{
+        $img = \imagecreatefrompng($path);
+        [$k, $l] = \getimagesize($path);
+        $bytes = "";
+
+        for ($y = 0; $y < $l; ++$y) {
+            for ($x = 0; $x < $k; ++$x) {
+                $argb = \imagecolorat($img, $x, $y);
+                $bytes .= \chr(($argb >> 16) & 0xff).\chr(($argb >> 8) & 0xff).\chr($argb & 0xff).\chr((~($argb >> 24) << 1) & 0xff);
+            }
+        }
+
+        \imagedestroy($img);
+        return $bytes;
     }
 }
